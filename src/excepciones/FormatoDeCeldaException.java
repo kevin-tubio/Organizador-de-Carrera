ed2ea134ce2254ec0291dfecfc00639adebd0c5b
Kevin Tubio <kevin.tubio@hotmail.com>
2021-07-26 05:46:54 -0300

    Refactor del InterpretadorDeArchivos, pruebas pasando

---
+++ src/excepciones/FormatoDeCeldaException.java
@@ -0,0 +1,15 @@
+package excepciones;
+
+public class FormatoDeCeldaException extends PlanillaInvalidaException {
+
+	private static final long serialVersionUID = -3370512511107656679L;
+
+	public FormatoDeCeldaException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public FormatoDeCeldaException(String message) {
+		super(message);
+	}
+
+}
--- src/sistema/InterpretadorDeArchivos.java
+++ src/sistema/InterpretadorDeArchivos.java
@@ -2,76 +2,102 @@ package sistema;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.Iterator;
 
-import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.poifs.filesystem.OfficeXmlFileException;
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import excepciones.FormatoDeCeldaException;
 import excepciones.PlanillaInvalidaException;
 import listado.Listado;
 import listado.Materia;
+import listado.Materia.Estado;
 
 public class InterpretadorDeArchivos {
 
 	public Listado generarListado(String ruta) throws IOException, PlanillaInvalidaException {
 		var listado = Listado.obtenerListado();
-		Sheet hoja = obtenerSheet(ruta);
-		validarPlanilla(hoja);
+		var hoja = obtenerHoja(ruta);
+
 		agregarMaterias(listado, hoja);
 		return listado;
 	}
 
-	private void validarPlanilla(Sheet hoja) {
-		
-	}
-
-	private Sheet obtenerSheet(String ruta) throws IOException {
+	private Sheet obtenerHoja(String ruta) throws IOException {
 		try (var libro = new HSSFWorkbook(new FileInputStream(ruta))) {
-			HSSFSheet hoja = libro.getSheetAt(0);
-			return hoja;
-		} catch(OfficeXmlFileException e) {
+			return libro.getSheetAt(0);
+		} catch (OfficeXmlFileException e) {
 			try (var libro = new XSSFWorkbook(new FileInputStream(ruta))) {
-				XSSFSheet hoja = libro.getSheetAt(0);
-				return hoja;
+				return libro.getSheetAt(0);
 			}
 		}
 	}
 
-	private void agregarMaterias(Listado listado, Sheet hoja) {
+	private void agregarMaterias(Listado listado, Sheet hoja) throws PlanillaInvalidaException {
 		Iterator<Row> iteradorFilas = hoja.iterator();
-		Iterator<Cell> iteradorColumnas = null;
 		while (iteradorFilas.hasNext()) {
 			Row filaActual = iteradorFilas.next();
-			if (filaActual.getLastCellNum() == 1 || filaActual.getCell(0).getStringCellValue().equals("Actividad"))
-				continue;
-
-			iteradorColumnas = filaActual.cellIterator();
-			ArrayDeque<String> colaDeDatos = new ArrayDeque<>();
-			while (iteradorColumnas.hasNext()) {
-				colaDeDatos.offer(iteradorColumnas.next().getStringCellValue());
+			if (filaActual.getCell(0).getStringCellValue().matches("^[A-Za-zÀ-ÿ]+[A-Za-zÀ-ÿ,. ]+ \\(\\d+\\)")) {
+				try {
+					listado.agregarMateria(crearMateria(filaActual));
+				} catch (FormatoDeCeldaException e) {
+					System.err.println("Fila " + (filaActual.getRowNum() + 1) + ", " + e.getMessage());
+				}
 			}
-
-			listado.agregarMateria(crearMateria(colaDeDatos));
+		}
+		if (listado.consultarCantidadDeMaterias() == 0) {
+			throw new PlanillaInvalidaException("No se ha podido interpretar las materias de la planilla provista");
 		}
 	}
 
-	public Materia crearMateria(Deque<String> colaDeDatos) {
-		String cadena = colaDeDatos.peek();
-		var numeroActividad = Integer.parseInt(cadena.split("[^\\d]+")[1]);
-		String nombre = colaDeDatos.poll().split("[\\d\\(\\)\\n]+")[0].strip();
-		colaDeDatos.poll();
-		var anio = Integer.parseInt(colaDeDatos.poll());
-		var periodo = Integer.parseInt(colaDeDatos.poll().split("[^\\d]")[0]);
+	private Materia crearMateria(Row filaActual) throws FormatoDeCeldaException {
+		var numero = obtenerNumeroMateria(filaActual.getCell(0).getStringCellValue());
+		var nombre = obtenerNombreMateria(filaActual.getCell(0).getStringCellValue());
+		var anio = obtenerAnioDeMateria(filaActual.getCell(2).getStringCellValue());
+		var periodo = obtenerPeriodoDeMateria(filaActual.getCell(3).getStringCellValue());
+		var materia = new Materia(numero, nombre, anio, periodo);
+		obtenerEstadoDeMateria(materia, filaActual.getCell(4).getStringCellValue());
+		return materia;
+	}
 
-		return new Materia(numeroActividad, nombre, anio, periodo);
+	private int obtenerNumeroMateria(String contenido) {
+		return Integer.parseInt(contenido.split("[^\\d]+")[1]);
+	}
+
+	private String obtenerNombreMateria(String contenido) {
+		return contenido.split("[\\d\\(\\)\\n]+")[0].strip();
+	}
+
+	private int obtenerAnioDeMateria(String contenido) throws FormatoDeCeldaException {
+		try {
+			return Integer.parseInt(contenido);
+		} catch (NumberFormatException e) {
+			throw new FormatoDeCeldaException("columna 3. Se esperaba un numero.", e.getCause());
+		}
+	}
+
+	private int obtenerPeriodoDeMateria(String contenido) throws FormatoDeCeldaException {
+		try {
+			return Integer.parseInt(contenido.split("[^\\d]")[0]);
+		} catch (NumberFormatException e) {
+			throw new FormatoDeCeldaException("columna 4. Se esperaba un numero.", e.getCause());
+		}
+	}
+
+	private void obtenerEstadoDeMateria(Materia materia, String contenido) throws FormatoDeCeldaException {
+		if (contenido.equals("") || contenido.matches("[\s]+")) {
+			materia.setEstado(Estado.NO_CURSADA);
+		} else if (contenido.matches("^\\d{1,2} \\([A][a-z]+\\)")) {
+			materia.setCalificacion(Integer.parseInt(contenido.split(" \\([Aa-z]+\\)")[0]));
+			materia.setEstado(Estado.APROBADA);
+		} else if (contenido.matches("^[A][a-z]+ \\([A][a-z]+\\)")) {
+			materia.setEstado(Estado.REGULARIZADA);
+		} else {
+			throw new FormatoDeCeldaException("columna 5. Se esperaba un estado de cursada valido o una celda vacia.");
+		}
 	}
 
 }
