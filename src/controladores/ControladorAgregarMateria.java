afaff00d21cf540f89e52c93fddb8abfd76407d1
Kevin Tubio <kevin.tubio@hotmail.com>
2021-08-13 20:45:27 -0300

    inicalizacion de botones, spinners, choiceboxes y textfields

--- src/controladores/ControladorAgregarMateria.java
+++ src/controladores/ControladorAgregarMateria.java
@@ -3,11 +3,78 @@ package controladores;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import enumerados.Estado;
+import enumerados.Periodo;
+import enumerados.Tipo;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class ControladorAgregarMateria implements Initializable {
 
+	@FXML
+	private Button aceptar;
+	@FXML
+	private Button cancelar;
+	@FXML
+	private TextField id;
+	@FXML
+	private TextField nombre;
+	@FXML
+	private ChoiceBox<Tipo> tipo;
+	@FXML
+	private ChoiceBox<Periodo> periodo;
+	@FXML
+	private ChoiceBox<Estado> estado;
+	@FXML
+	private Spinner<Integer> nota;
+	@FXML
+	private Spinner<Integer> anio;
+	@FXML
+	private Spinner<Integer> hs;
+	@FXML
+	private Spinner<Double> creditos;
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		this.inicializarChoiceBoxes();
+		this.inicializarSpinners();
+		this.inicializarBotones();
 	}
+
+	private void inicializarBotones() {
+		this.aceptar.disableProperty().bind(this.id.textProperty().isEmpty().or(this.nombre.textProperty().isEmpty()));
+		this.cancelar.setCancelButton(true);
+	}
+
+	private void inicializarChoiceBoxes() {
+		this.tipo.getItems().addAll(Tipo.values());
+		this.tipo.setValue(Tipo.MATERIA);
+		this.periodo.getItems().addAll(Periodo.values());
+		this.periodo.setValue(Periodo.PRIMER_CUATRIMESTRE);
+		this.estado.getItems().addAll(Estado.values());
+		this.estado.setValue(Estado.NO_CURSADA);
+	}
+
+	private void inicializarSpinners() {
+		this.nota.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(4, 10));
+		this.nota.disableProperty()
+				.bind(estado.getSelectionModel().selectedItemProperty().isNotEqualTo(Estado.APROBADA));
+		this.anio.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 10));
+		this.anio.getValueFactory().setValue(1);
+		this.hs.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 20));
+		this.hs.getValueFactory().setValue(4);
+		this.creditos.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(0.0, 20.0));
+		this.creditos.getValueFactory().setValue(0.0);
+	}
+
+	public void cancelar() {
+		((Stage) this.cancelar.getScene().getWindow()).close();
+	}
+
 }
