9695c620b8d61fe38f6801c3183ca0e3cdb86796
Kevin Tubio <kevin.tubio@hotmail.com>
2021-08-14 13:42:55 -0300

    las materias del listado ahora se muestran en la lista de la pestaña lista de materias

--- src/controladores/ControladorLista.java
+++ src/controladores/ControladorLista.java
@@ -3,13 +3,43 @@ package controladores;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.MapChangeListener;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import listado.Listado;
+import listado.Materia;
 
 public class ControladorLista implements Initializable {
+
 	private ControladorPrincipal controlador;
+	private FilteredList<Materia> listaFiltrada;
+	private ObservableList<Materia> listaDeMaterias;
+	@FXML
+	private ListView<Materia> lista;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		this.lista.setPlaceholder(new Label("No hay materias cargadas"));
+		this.agregarSubscriptorAListado();
+		this.listaFiltrada = new FilteredList<>(this.listaDeMaterias);
+		this.lista.setItems(this.listaFiltrada);
+	}
+
+	private void agregarSubscriptorAListado() {
+		this.listaDeMaterias = FXCollections.observableArrayList();
+		MapChangeListener<Integer, Materia> subscriptor = cambio -> {
+			if (cambio.wasAdded()) {
+				this.listaDeMaterias.add(cambio.getValueAdded());
+			} else {
+				this.listaDeMaterias.remove(cambio.getValueRemoved());
+			}
+		};
+		Listado.obtenerListado().getListadoDeMaterias().addListener(subscriptor);
 	}
 
 	public void inyectar(ControladorPrincipal controladorPrincipal) {
