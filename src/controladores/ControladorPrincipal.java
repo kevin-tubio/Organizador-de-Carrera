85cfc26ae6be995edbc5110857b787989527111a
Kevin Tubio <kevin.tubio@hotmail.com>
2021-08-13 21:03:53 -0300

    ahora se pueden cargar archivos desde el boton abrir del menu archivo, las materias cargadas se muestran en la tabla

--- src/controladores/ControladorPrincipal.java
+++ src/controladores/ControladorPrincipal.java
@@ -1,10 +1,18 @@
 package controladores;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import excepciones.ArchivoException;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import listado.Listado;
+import sistema.InterpretadorDeArchivos;
+import sistema.InterpretadorDeDatosGuardados;
+import sistema.InterpretadorDePlanillas;
 
 public class ControladorPrincipal implements Initializable {
 
@@ -17,6 +25,29 @@ public class ControladorPrincipal implements Initializable {
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		this.planDeEstudiosController.inyectar(this);
 		this.listaDeMateriasController.inyectar(this);
+	}
+
+	public void abrirArchivo() {
+		var fileChooser = new FileChooser();
+		fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Excel Files", "*.xls", "*.xlsx"),
+				new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+		try {
+			var archivo = fileChooser.showOpenDialog(new Stage());
+			Listado.borrarListado();
+			cargarArchivo(archivo);
+		} catch (ArchivoException e) {
+			System.out.println("intenta con otro archivo");
+		}
+	}
+
+	private void cargarArchivo(File archivo) throws ArchivoException {
+		InterpretadorDeArchivos interpretador = null;
+		if (archivo.getName().endsWith(".txt")) {
+			interpretador = new InterpretadorDeDatosGuardados();
+		} else {
+			interpretador = new InterpretadorDePlanillas();
+		}
+		interpretador.generarListado(archivo.getAbsolutePath());
 	}
 
 }
--- src/controladores/ControladorTabla.java
+++ src/controladores/ControladorTabla.java
@@ -3,13 +3,60 @@ package controladores;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.collections.MapChangeListener;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import listado.Listado;
+import listado.Materia;
 
 public class ControladorTabla implements Initializable {
+
 	private ControladorPrincipal controlador;
+
+	@FXML
+	private TableView<Materia> tabla;
+	@FXML
+	private TableColumn<Materia, Integer> numeroActividad;
+	@FXML
+	private TableColumn<Materia, String> nombreActividad;
+	@FXML
+	private TableColumn<Materia, Integer> anio;
+	@FXML
+	private TableColumn<Materia, String> periodo;
+	@FXML
+	private TableColumn<Materia, String> nota;
+	@FXML
+	private TableColumn<Materia, String> estado;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		this.inicializarTabla();
+		this.agregarSubscriptorAListado();
+	}
+
+	private void inicializarTabla() {
+		numeroActividad.setCellValueFactory(new PropertyValueFactory<>("numeroActividad"));
+		nombreActividad.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+		anio.setCellValueFactory(new PropertyValueFactory<>("anio"));
+		periodo.setCellValueFactory(new PropertyValueFactory<>("periodo"));
+		nota.setCellValueFactory(new PropertyValueFactory<>("calificacion"));
+		estado.setCellValueFactory(new PropertyValueFactory<>("estado"));
+		tabla.setPlaceholder(new Label("No hay materias cargadas"));
+	}
+
+	private void agregarSubscriptorAListado() {
+		MapChangeListener<Integer, Materia> subscriptor = cambio -> {
+			if (cambio.wasAdded()) {
+				this.tabla.getItems().add(cambio.getValueAdded());
+			} else {
+				this.tabla.getItems().remove(cambio.getValueRemoved());
+			}
+		};
+		Listado.obtenerListado().getListadoDeMaterias().addListener(subscriptor);
 	}
 
 	public void inyectar(ControladorPrincipal controladorPrincipal) {
