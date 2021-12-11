98b086ea1f41913cb673b13e1a1307a9c62feff6
Kevin Eloy Tubio <kevtubio@emeal.nttdata.com>
2021-12-11 17:43:03 -0300

    Refactor metodo start App

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -12,8 +11,8 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage stage) throws IOException {
-		Parent root = FXMLLoader.load(getClass().getResource("../fxml/Main.fxml"));
-		var scene = new Scene(root);
+		var loader = new FXMLLoader(this.getClass().getResource("../fxml/Main.fxml"));
+		var scene = new Scene(loader.load());
 		stage.setTitle("Organizador");
 		stage.setScene(scene);
 		stage.show();
